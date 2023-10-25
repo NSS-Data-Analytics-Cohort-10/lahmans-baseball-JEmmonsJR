1da79ea44962872023cfce60a05e73fab72704d1
JEmmonsJR <jdejr73@gmail.com>
2023-10-24 19:29:42 -0500

    Completed Question 3

--- scripts/lahman_exercise.sql
+++ scripts/lahman_exercise.sql
@@ -4,19 +4,66 @@
 
 --Use SQL queries to find answers to the *Initial Questions*. If time permits, choose one (or more) of the *Open-Ended Questions*. Toward the end of the bootcamp, we will revisit this data if time allows to combine SQL, Excel Power Pivot, and/or Python to answer more of the *Open-Ended Questions*.
 
-
+select * from appearances where playerid = 'gaedeed01'
 
 -- **Initial Questions**
 
 -- 1. What range of years for baseball games played does the provided database cover? 
 
+SELECT
+	MIN(year),
+	MAX(year)
+FROM homegames;
+
+--ANSWER: The earliest is 1871 and the latest is 2016
+
 -- 2. Find the name and height of the shortest player in the database. How many games did he play in? What is the name of the team for which he played?
-   
+
+SELECT
+	DISTINCT p.namegiven
+	,p.height
+	,f.franchname
+	,(a.g_all) AS games_played
+FROM people AS p
+INNER JOIN appearances AS a
+USING(playerid)
+LEFT JOIN teams AS t
+USING(teamid)
+LEFT JOIN teamsfranchises AS f
+USING(franchid)
+WHERE p.height = (SELECT MIN(height)
+				 FROM people)
+
+--ANSWERS: Edward Carl 43inches and 1 game for the Baltimore Orioles	
 
 -- 3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
 	
+SELECT
+	DISTINCT c.playerid
+	,p.namefirst
+	,p.namelast
+	,SUM(sl.salary)AS tot_salary
+FROM schools AS s
+INNER JOIN collegeplaying AS c
+USING(schoolid)
+LEFT JOIN people AS p
+USING(playerid)
+LEFT JOIN salaries AS SL
+USING(playerid)
+WHERE s.schoolname = 'Vanderbilt University' AND sl.salary IS NOT NULL
+GROUP BY c.playerid, p.namefirst, p.namelast, s.schoolname
+ORDER BY tot_salary DESC
+LIMIT 1
+
+--ANSWERS: David Price $245,553,888
 
 -- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
+   
+WITH field_group AS(
+	SELECT
+		CASE
+			WH
+)
    
 -- 5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
    
