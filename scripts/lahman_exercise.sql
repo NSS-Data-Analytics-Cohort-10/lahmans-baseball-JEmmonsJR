d0c8cf0ed01d8d0549824d16f961769f41cc35e9
JEmmonsJR <jdejr73@gmail.com>
2023-10-31 19:23:20 -0500

    Completed Question 11

--- scripts/lahman_exercise.sql
+++ scripts/lahman_exercise.sql
@@ -284,11 +284,32 @@ AND b.hr >= 1
 
 -- 11. Is there any correlation between number of wins and team salary? Use data from 2000 and later to answer this question. As you do this analysis, keep in mind that salaries across the whole league tend to increase together, so you may want to look on a year-by-year basis.
 
+WITH sal_rank AS(
+SELECT
+	yearid
+	,teamid
+	,SUM(salary)::numeric::money AS team_pay
+	, RANK() OVER(PARTITION BY yearid ORDER BY SUM(salary) DESC)
+FROM salaries
+WHERE yearid >= 2000
+GROUP BY teamid, yearid
+)
+SELECT
+	s.rank
+	,t.w
+FROM sal_rank AS s
+LEFT JOIN teams AS t
+USING(yearid, teamid)
+ORDER BY s.rank, t.w DESC
+
+--No Correlation
+
 -- 12. In this question, you will explore the connection between number of wins and attendance.
 --     <ol type="a">
 --       <li>Does there appear to be any correlation between attendance at home games and number of wins? </li>
 --       <li>Do teams that win the world series see a boost in attendance the following year? What about teams that made the playoffs? Making the playoffs means either being a division winner or a wild card winner.</li>
 --     </ol>
+
 
 
 -- 13. It is thought that since left-handed pitchers are more rare, causing batters to face them less often, that they are more effective. Investigate this claim and present evidence to either support or dispute this claim. First, determine just how rare left-handed pitchers are compared with right-handed pitchers. Are left-handed pitchers more likely to win the Cy Young Award? Are they more likely to make it into the hall of fame?
